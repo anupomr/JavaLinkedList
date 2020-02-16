cfcf830749643ce589295150da386a561d7ce54a
anupo <anupo@10.44.39.4>
2020-02-16 15:01:27 -0500

    Write add, remove and find function.

--- JavaLinkedList/src/singly/linked/list/SimpleSLL.java
+++ JavaLinkedList/src/singly/linked/list/SimpleSLL.java
@@ -36,6 +36,65 @@ public class SimpleSLL {
 			return this.nextNode;
 		}
 	}
+	
+	Node root;
+	int size;
+
+	public SimpleSLL() {
+		root = new Node();
+		size = 0;
+	}
+	public void setSize(int s) {
+		this.size = s;
+	}
+
+	public int getSize() {
+		return this.size;
+	}
+
+	public Node add(int data) {
+		Node newNode = new Node(data, root);
+		this.root = newNode;
+		this.size++;
+		return newNode;
+	}
+
+	public Node find(int data) {
+		Node thisNode = this.root;
+
+		while (thisNode != null) {
+			if (thisNode.getData() == data)
+				return thisNode;
+			thisNode = thisNode.getNextNode();
+		}
+		return null;
+	}
+
+	public boolean remove(int data) {
+		Node thisNode = this.root;
+		Node prevNode = null;
+
+		while (thisNode != null) {
+			if (thisNode.getData() == data) {
+				if (prevNode != null)
+					prevNode.setNextNode(thisNode.getNextNode());
+				else
+					this.root = null;
+				this.setSize(this.getSize() - 1);
+				return true;
+			}
+			prevNode = thisNode;
+			thisNode = thisNode.getNextNode();
+		}
+		return false;
+	}
+
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
 
