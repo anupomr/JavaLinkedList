91892dd57ceef90674d858c86cc35e8aec04acb4
anupo <anupo@10.44.39.4>
2020-02-16 15:04:14 -0500

    Test code -on main function

--- JavaLinkedList/src/singly/linked/list/SimpleSLL.java
+++ JavaLinkedList/src/singly/linked/list/SimpleSLL.java
@@ -94,9 +94,19 @@ public class SimpleSLL {
 	
 	
 	
-	
+	// Test code -on main function
 	public static void main(String[] args) {
-		
+		SimpleSLL ll = new SimpleSLL();
+		System.out.println(ll.getSize());
+		ll.add(8);
+		System.out.println(ll.getSize());
+		ll.add(17);
+		ll.add(5);
+		ll.add(10);
+		System.out.println(ll.find(17).getData());
+		ll.remove(5);
+		System.out.println(ll.getSize());
+		System.out.println(ll.find(5));
 
 	}
 
