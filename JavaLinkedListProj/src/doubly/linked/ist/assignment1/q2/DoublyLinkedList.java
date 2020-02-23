f27cf8fad5095b17273e5bc6b715a9fb555355ea
anupo <anupo@192.168.2.24>
2020-02-22 23:29:56 -0500

    Two private method written for add and remove.

---
+++ JavaLinkedListProj/src/doubly/linked/ist/assignment1/q2/DoublyLinkedList.java
@@ -0,0 +1,49 @@
+package doubly.linked.ist.assignment1.q2;
+
+public class DoublyLinkedList <E>{
+	//nested Node class
+		private static class Node<E>{
+			private E element;
+			private Node<E> prev;
+			private Node<E> next;
+			public Node (E e, Node<E> p, Node<E> n){
+				element = e;
+				prev = p;
+				next = n;
+			}
+		    //public methods
+			public E getElement() {return element;}
+			public Node<E> getPrev() {return prev;}
+			public Node<E> getNext() {return next;}
+			public void setPrev(Node<E> p) {prev = p;}
+			public void setNext(Node<E> n) {next = n;}
+		}
+		
+		private Node<E> header;
+		private Node<E> trailer;
+		private int size = 0;
+
+		//constructs
+		public DoublyLinkedList() {
+			header = new Node<>(null,null,null);
+			trailer = new Node<>(null, header, null);
+			header.setNext(trailer);
+		}
+		
+		private void addBetween(E e, Node<E> predecessor, Node<E> successor){
+			//create a node
+			Node<E> newest = new Node<>(e, predecessor, successor);
+			predecessor.setNext(newest);
+			successor.setPrev(newest);
+			size++;
+		}
+
+		private E remove(Node<E> node){
+			Node<E> predecessor = node.getPrev();
+			Node<E> successor = node.getNext();
+			predecessor.setNext(successor);
+			successor.setPrev(predecessor);
+			size--;
+			return node.getElement();
+		}
+}
