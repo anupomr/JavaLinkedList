b1438724d0d2e902ec9dcdf935161d8c6e977235
anupo <anupo@192.168.2.24>
2020-02-23 07:25:10 -0500

    Add and remove function added

--- JavaLinkedListProj/src/doubly/linked/ist/assignment1/q2/DoublyLinkedList.java
+++ JavaLinkedListProj/src/doubly/linked/ist/assignment1/q2/DoublyLinkedList.java
@@ -31,7 +31,6 @@ public class DoublyLinkedList <E>{
 		}
 		
 		private void addBetween(E e, Node<E> predecessor, Node<E> successor){
-			//create a node
 			Node<E> newest = new Node<>(e, predecessor, successor);
 			predecessor.setNext(newest);
 			successor.setPrev(newest);
@@ -46,4 +45,27 @@ public class DoublyLinkedList <E>{
 			size--;
 			return node.getElement();
 		}
+		
+		public int size() {return size;}
+		
+		public boolean isEmpty() {return size == 0;}
+		
+		public E first() {
+			if (isEmpty()) return null;
+			return header.getNext().getElement();
+		}
+		
+		public E last() {
+			if (isEmpty()) return null;
+			return trailer.getPrev().getElement();
+		}
+
+		public void addFirst(E e){
+			addBetween(e, header, header.getNext());
+		}
+
+		public void addLast(E e){
+			addBetween(e, trailer.getPrev(), trailer);
+		}
+
 }
